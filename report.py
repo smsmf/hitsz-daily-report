2f2c1cc00c5b75e698ced54bc761b0d4b4f14770
maizhouzi <maizhouzi@qq.com>
2021-05-04 16:59:31 +0800

    更新

--- report.py
+++ report.py
@@ -24,6 +24,9 @@ class ReportException(Exception):
     class SubmitError(Exception):
         """上报失败"""
 
+    class ReportExistError(Exception):
+        """已经上报"""
+
 
 class Report(object):
     def __init__(self, args):
@@ -33,9 +36,12 @@ class Report(object):
             raise ReportException("请先设置 Actions Secrets！")
 
         self.session = requests.session()
+        self.proxies = self.set_vpn_port(1080)
+
         self.username = args.username
         self.password = args.password
         self.graduating = '1' if args.graduating else '0'
+
         logging.info(f"{'' if args.graduating else '非'}毕业班学生，微信提醒{'开启' if args.sckey else '关闭'}。")
 
         self.urls = {
@@ -57,6 +63,12 @@ class Report(object):
         ]
 
     @staticmethod
+    def set_vpn_port(port):
+        socks5 = f"socks5h://127.0.0.1:{port}"
+        proxies = {"http": socks5, "https": socks5}
+        return proxies
+
+    @staticmethod
     def new_session():
         sess = requests.session()
         sess.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
@@ -68,7 +80,7 @@ class Report(object):
 
         self.session = self.new_session()
         url_sso = self.urls['sso']
-        response = self.session.get(url_sso)
+        response = self.session.get(url_sso, proxies=self.proxies)
         jsessionid = dict_from_cookiejar(response.cookies)['JSESSIONID']
         url_login = self.urls['login'].format(jsessionid)
         logging.debug(f'GET {url_sso} {response.status_code}')
@@ -87,7 +99,7 @@ class Report(object):
         }
 
         # 禁用跳转，用于处理登录失败的问题
-        response = self.session.post(url_login, params=params, allow_redirects=False)
+        response = self.session.post(url_login, params=params, allow_redirects=False, proxies=self.proxies)
         logging.debug(f'POST {url_login} {response.status_code}')
 
         if response.status_code == 200:
@@ -98,7 +110,7 @@ class Report(object):
 
         # 登录成功，继续跳转，更新 cookie
         next_url = response.next.url
-        response = self.session.get(next_url)
+        response = self.session.get(next_url, proxies=self.proxies)
         logging.debug(f'GET {next_url} {response.status_code}')
 
         logging.info(f"认证系统登录成功。")
@@ -108,7 +120,7 @@ class Report(object):
 
         # 查询今天是否已生成上报信息，并获得 ID
         url_csh = self.urls['csh']
-        response = self.session.post(url_csh)
+        response = self.session.post(url_csh, proxies=self.proxies)
         result = response.json()
         logging.debug(f'POST {url_csh} {response.status_code}')
 
@@ -116,16 +128,16 @@ class Report(object):
             logging.warning("新增每日上报信息失败！")
 
             url_check = self.urls['check']
-            response = self.session.post(url_check)
+            response = self.session.post(url_check, proxies=self.proxies)
             today_report = response.json()['module']['data'][0]
             logging.debug(f'POST {url_check} {response.status_code}')
 
             if today_report['zt'] == '00':
                 logging.warning("上报信息已存在，尚未提交。")
             elif today_report['zt'] == '01':
-                raise ReportException.SubmitError("上报信息已提交，待审核。")
+                raise ReportException.ReportExistError("上报信息已提交，待审核。")
             elif today_report['zt'] == '02':
-                raise ReportException.SubmitError("上报信息已审核，无需重复提交。")
+                raise ReportException.ReportExistError("上报信息已审核，无需重复提交。")
             else:
                 raise ReportException.SubmitError(f"上报失败，zt：{today_report['zt']}")
 
@@ -137,7 +149,7 @@ class Report(object):
         # 获取每日上报信息的模板
         url_msg = self.urls['get']
         params = {'info': json.dumps({'id': module})}
-        response = self.session.post(url_msg, params=params)
+        response = self.session.post(url_msg, params=params, proxies=self.proxies)
         data_orig = response.json()['module']['data'][0]
         logging.debug(f'POST {url_msg} {response.status_code}')
 
@@ -148,7 +160,7 @@ class Report(object):
         logging.info(f"生成上报信息成功。今日体温：{temperature}℃")
 
         url_save = self.urls['save']
-        response = self.session.post(url_save, params=report_info)
+        response = self.session.post(url_save, params=report_info, proxies=self.proxies)
         logging.debug(f'POST {url_save} {response.status_code}')
 
         if not response.json().get('isSuccess'):
@@ -166,18 +178,26 @@ def main(args):
     r = Report(args)
 
     try:
-        wait_a_minute("准备就绪，等待{}秒后运行。")
         r.student_login()
     except ReportException.LoginError:
-        wait_a_minute("登录失败，等待{}秒后重试。", 1)
+        wait_a_minute("登录失败，将在 {} 秒后重试。", 1)
+        r.student_login()
+    except Exception as err:
+        logging.error(err)
+        wait_a_minute("切换代理，将在 {} 秒后重试。")
+        r.proxies = r.set_vpn_port(2080)
         r.student_login()
 
-    module_id = r.student_report_check()
+    try:
+        module_id = r.student_report_check()
+    except ReportException.ReportExistError as err:
+        logging.error(err)
+        return
 
     try:
         r.student_report_submit(module_id)
     except ReportException.SubmitError:
-        wait_a_minute("提交失败，等待{}秒后重试。", 1)
+        wait_a_minute("提交失败，将在 {} 秒后重试。", 1)
         r.student_report_submit(module_id)
 
 
@@ -194,18 +214,21 @@ if __name__ == '__main__':
     except ReportException.LoginError as e:
         report_msg = f"登陆失败！原因：{e}"
         logging.error(report_msg)
+        raise ReportException(report_msg)
     except ReportException.SubmitError as e:
         report_msg = f"上报失败！原因：{e}"
         logging.error(report_msg)
+        raise ReportException(report_msg)
     except Exception as e:
         report_msg = f"上报失败！其他错误：{e}"
         logging.critical(report_msg)
+        raise ReportException(report_msg)
     else:
         report_msg = f"今日疫情状态上报成功。"
         logging.warning(report_msg)
-
-    if arguments.sckey:
+    finally:
         current = datetime.today().strftime('%Y-%m-%d_%H:%M:%S')
-        requests.get(f"https://api.day.app/{arguments.sckey}/{report_msg}{current}")
-        logging.info("BARK消息已发送。")
+        if arguments.sckey:
+            requests.get(f"https://api.day.app/{arguments.sckey}/{report_msg}{current}")
+            logging.info("BARK消息已发送。")
 
